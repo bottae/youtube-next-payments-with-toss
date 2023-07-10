ed1d285ff65334424a942730d5ff0b514bec786f
이재하(홈퍼니싱개발팀) <dcom@hanssem.com>
2023-07-10 16:57:13 +0900

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-"use client";
-import { loadTossPayments } from "@tosspayments/payment-sdk";
+'use client';
+import { loadTossPayments } from '@tosspayments/payment-sdk';
 
 export default function Page() {
   const handleClick = async () => {
@@ -7,10 +7,10 @@ export default function Page() {
       process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY
     );
 
-    await tossPayments.requestPayment("카드", {
+    await tossPayments.requestPayment('카드', {
       amount: 5000,
       orderId: Math.random().toString(36).slice(2),
-      orderName: "맥북",
+      orderName: '맥북',
       successUrl: `${window.location.origin}/api/payments`,
       failUrl: `${window.location.origin}/api/payments/fail`,
     });
